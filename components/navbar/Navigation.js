74da0493452e4ba546f7befc06333ff9dac721f5
Yeganeh <vnus.daneshparvar@gmail.com>
2023-11-19 22:03:02 -0500

    added about us

--- components/navbar/Navigation.js
+++ components/navbar/Navigation.js
@@ -4,18 +4,20 @@ import { NavigationContainer } from '@react-navigation/native'
 
 import HomeScreen from '../../screens/HomeScreen.js'
 import FavoritesScreen from '../../screens/FavoritesScreen.js'
+import About from '../../screens/AboutUs.js'
  
 const Tab = createBottomTabNavigator();
 
-function AppNavigator() {
+function Navigation() {
     return (
       <NavigationContainer>
         <Tab.Navigator>
           <Tab.Screen name="Home" component={HomeScreen} />
           <Tab.Screen name="Favorites" component={FavoritesScreen} />
+          <Tab.Screen name="About Us" component={About} />
         </Tab.Navigator>
       </NavigationContainer>
     );
   }
   
-  export default AppNavigator;
+  export default Navigation;
---
+++ screens/AboutUs.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Text } from 'react-native';
+
+const AboutUs = () => {
+  return (
+    <Text>AboutUs</Text>
+  )
+}
+export default AboutUs;
